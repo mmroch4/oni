055b13b12395e275b27ff5d92472c18367ffd5dc
Miguel Rocha <miguelrocha.dev@gmail.com>
2025-04-23 17:01:14 +0100

    feat: add problem finals 2021 A part I

--- problems/finals/2021/A/index.cpp
+++ problems/finals/2021/A/index.cpp
@@ -2,16 +2,51 @@
 
 using namespace std;
 
-int fact(int n) {
+int main() {
+  int p, n, v;
+  vector<int> mountains;
+
+  cin >> p >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> v;
+
+    mountains.push_back(v);
+  }
+  
+  stable_sort(mountains.begin(), mountains.end());
+
+  int counter = 1;
+  int acc = 0;
+
+  for (int i = 1; i < n; i++)
+  {
+    if (mountains[i] == mountains[i - 1]) {
+      acc += counter;
+      counter++;
+    }
+    else {
+      counter = 1;
+    }
+  }
+  
+  cout << acc << "\n";
+}
+
+
+/*
+
+long long fact(int n) {
   if (n <= 1) {
     return 1;
   }
 
-  int acc = 1;
+  long long acc = 1;
 
-  for (int i = 2; i <= n; i++)
+  for (int i = 2; i <= (long long)n; i++)
   {
-    acc *= i;
+    acc *= (long long)i;
   }
 
   return acc;
@@ -35,7 +70,7 @@ int main() {
     int counter = -1;
     int current = -1;
 
-    int acc = 0;
+    long long acc = 0;
 
     for (int i = 0; i < n; i++) {
       if (values[i] == current) {
@@ -60,13 +95,82 @@ int main() {
   }
   else {
     int acc = n;
+    int m = 0;
+    vector<int> odd_nums, even_nums;
 
-    for (int i = 0; i < n; i++)
-    {
-      /* code */
+    for (int i = 0; i < n; i++) {
+      if (i % 2 == 0) {
+        even_nums.push_back(values[i]);
+      }
+      else {
+        odd_nums.push_back(values[i]);
+      }
+    }
+    
+    
+    for (int i = 0; i < (int)even_nums.size() - 1; i++) {
+      int uni_i = 2 * i;
+
+      // cout << "RODADA: " << i << ":\n";
+      for (int p = i + 1; p < (int)even_nums.size(); p++) {
+        int uni_p = 2 * p;
+        int middle = uni_i + floor((uni_p - uni_i) / 2);
+        
+        // cout << "FROM: " << i << " (" << uni_i << ") TO: " << p << " (" << uni_p << ") \n";
+
+        auto r = *max_element(values.begin() + uni_i, values.begin() + middle);
+        auto l = *max_element(values.begin() + middle + 1, values.begin() + uni_p + 1);
+
+        if (r < values[middle] && l < values[middle]) {
+          acc++;
+
+          int range = uni_p - uni_i + 1;
+          // cout << "---- MATCH -----" << "\n"; 
+
+          if (range > m) {
+            m = range;
+          }
+        }
+
+        // cout << "LEFT: " << l << "RIGHT: " << r << " MIDDLE: " << values[middle] << endl;
+      }
+      
+      // cout << "--------------- \n";
+    }
+
+    for (int i = 0; i < (int)odd_nums.size() - 1; i++) {
+      int uni_i = 2 * i + 1;
+
+      // cout << "RODADA: " << i << ":\n";
+      for (int p = i + 1; p < (int)odd_nums.size(); p++) {
+        int uni_p = 2 * p + 1;
+        int middle = uni_i + floor((uni_p - uni_i) / 2);
+        
+        // cout << "FROM: " << i << " (" << uni_i << ") TO: " << p << " (" << uni_p << ") \n";
+
+        auto r = *max_element(values.begin() + uni_i, values.begin() + middle);
+        auto l = *max_element(values.begin() + middle + 1, values.begin() + uni_p + 1);
+
+        if (r < values[middle] && l < values[middle]) {
+          acc++;
+
+          // cout << "---- MATCH -----" << "\n"; 
+          
+          int range = uni_p - uni_i + 1;
+
+          if (range > m) {
+            m = range;
+          }
+        }
+
+        // cout << "MAIOR: " << r << " MIDDLE: " << values[middle] << endl;
+      }
+      
+      // cout << "--------------- \n";
     }
     
 
-    cout << acc << "\n";
+    cout << m << " " << acc << "\n";
   }
 }
+  */
