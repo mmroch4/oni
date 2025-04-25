3cd32361bcd47b89447b58c03ac265ca6d3c8200
Miguel Rocha <miguelrocha.dev@gmail.com>
2025-04-25 21:13:33 +0100

    feat: add problem final 2024 C part II

--- problems/finals/2024/C/index.cpp
+++ problems/finals/2024/C/index.cpp
@@ -1,81 +1,35 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-/**
- * 
- * def intersection(a, b):
-  [a_start, a_end] = a
-  [b_start, b_end] = b
-  
-  if a_start - b_end <= 0 and 0 <= a_end - b_start:    
-    start = a_start if 0 <= a_start - b_start else b_start
-    end = a_end if 0 <= b_end - a_end else b_end
+
+pair<bool, pair<int, int>> intersection(pair<int, int> a, pair<int, int> b) {
+  if (a.first - b.second <= 0 && 0 <= a.second - b.first)
+  {
+    int s = 0 <= a.first - b.first ? a.first : b.first;
+    int e = 0 <= b.second - a.second ? a.second : b.second;
     
-    return [start, end]
+    return {true, {s, e}};
+  }   
   
-  return False
- */
-
-int get_required_skills_to_win(int a, int m, int *skills, vector<int> *confronts, int *lost_to) {
-  vector<int> to_confront;
-
-  for (auto c : confronts[a]) {
-    to_confront.push_back(c);
-  }
-
-  while (to_confront.size() < m) {
-    vector<int> to_add = confronts[to_confront.back()];
-    
-    for (int u = to_confront.size(); u < to_add.size(); u++) {
-      to_confront.push_back(to_add[u]);
-    }
-  }
-
-  vector<vector<int>> intervals[to_confront.size()];
-
-  for (int z = 0; z < to_confront.size(); z++) {
-    int skill = skills[to_confront[z]];
-
-    vector<int> right, left;
-    bool has_left = false;
-    int s_r, e_r, s_l, e_l;
-
-    if (a < to_confront[z]) {
-      s_r = skill;
-    } 
-    else {
-      s_r = skill + 1;
-    }
-
-    e_r = skill + 999;
-
-    if (1000 <= skill) {
-      has_left = true;
-      s_l = 0;
-      e_l = skill - 1000;
-    }
-
-    right.push_back(s_r);
-    right.push_back(e_r);
-    intervals[z].push_back(right);
-
-    if (has_left) {
-      left.push_back(s_l);
-      left.push_back(e_l);
-      intervals[z].push_back(left);
-    }
-  }
-
-
-  for (auto g : intervals) {
-    for (auto e : g) {
-
-      cout << e[0] << " " << e[1] << "\n";
-    }
-  }
+  return {false, {-1, -1}};
 }
 
-int duel(int a, int b, int *skills) {
+vector<pair<int, int>> get_winning_ranges(int a, int b, vector<int>& skills) {
+  pair<int, int> l, r;
+
+  r = {a == b ? skills[a] : skills[a] + 1, skills[a] + 999};
+
+  if (skills[a] < 1000) 
+  {
+    return {r};
+  }
+
+  l = {0, skills[a] - 1000};
+
+  return {l, r};
+}
+
+int duel(int a, int b, vector<int>& skills) {
   int diff = abs(skills[a] - skills[b]);
 
   if (diff == 0) {
@@ -98,83 +52,100 @@ int duel(int a, int b, int *skills) {
   }
 }
 
-int get_champion(int n, int m, int *skills, vector<int> *confronts, int *lost_to) {
-  vector<int> competitors;
-
-  for (int i = 0; i < n; i++) {
-    competitors.push_back(i);
-  }
-
-  for (int i = 0; i < m; i++) {
-    vector<int> new_competitors;
-    int previous = -1;
-
-    for (auto c : competitors) {
-      if (previous == -1) {
-        previous = c;
-        continue;
-      }
-
-      int duel_winner = duel(previous, c, skills);
-
-      confronts[previous].push_back(c);
-      confronts[c].push_back(previous);
-
-      if (duel_winner == previous) {
-        lost_to[c] = previous;
-      } else {
-        lost_to[previous] = c;
-      }
-
-      new_competitors.push_back(duel_winner);
-
-      previous = -1;
-    }
-    
-    competitors = new_competitors;
-  }
-  
-  return competitors[0];
-}
-
 int main() {
   int p, n, s;
 
   cin >> p >> n;
 
-  int skills[n];
-  int lost_to[n];
-  vector<int> confronts[n];
-  int m = (int)log2(n);
+  vector<int> duels(2 * n);
+  vector<int> skills(n);
 
-
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cin >> s;
 
+    duels[n + i] = i;
     skills[i] = s;
-    lost_to[i] = -1;
   }
 
-  if (p == 1) {
-    int champion = get_champion(n, m, skills, confronts, lost_to);
-
-    cout << champion + 1 << "\n";
+  for (int u = n - 1; u > 0; u--)
+  {
+    duels[u] = duel(duels[2 * u], duels[2 * u + 1], skills);
   }
-  else if (p == 2) {
-    int champion = get_champion(n, m, skills, confronts, lost_to);
 
-    int required_skills_to_win[n];
+  if (p == 1) 
+  {
+    cout << duels[1] + 1 << "\n";
+  }
+  else if (p == 2) 
+  {
+    int winner = duels[1];
 
-    for (int i = 0; i < n; i++) {
-      // if (i == champion) {
-      //   required_skills_to_win[i] = skills[champion];
-      //   continue;
-      // }
+    for (int p = 0; p < n; p++)
+    {
+      if (p == winner) 
+      {
+        cout << (p != 0 ? " ": "") << skills[p];
+        continue;
+      }
 
-      //required_skills_to_win[i] =
-      if (i != 1) continue;
-      get_required_skills_to_win(i, m, skills, confronts, lost_to);
-    }
+      int i = n + p;
+      int k = i;
+      int rival;
+      int a = 0;
+      bool is_possible = true;
+
+      vector<pair<int, int>> ranges;
+
+      while (k > 1) {
+        rival = duels[k + (k % 2 == 0 ? 1 : -1)];
+
+        vector<pair<int, int>> winning_ranges = get_winning_ranges(rival, p, skills);
+        
+        if (a == 0)
+        {
+          for (auto r : winning_ranges)
+          {
+            ranges.push_back(r);
+          }
+        }
+        else {
+          vector<pair<int, int>> new_ranges;
+
+          for (auto w : ranges)
+          {
+            for (auto z : winning_ranges) 
+            {
+              auto intersects = intersection(w, z);
+
+              if (intersects.first) {
+                new_ranges.push_back(intersects.second);
+              }
+            }
+          }
+
+          if (new_ranges.size() == 0) {
+            is_possible = false;
+            break;
+          }
+
+          ranges = new_ranges;
+        }
+
+        k = floor(k / 2);
+        a++;
+      }
     
+      if (is_possible) 
+      {
+        cout << (p != 0 ? " ": "") << ranges[0].first;
+      }
+      else 
+      {
+        cout << (p != 0 ? " ": "") << -1;
+      }
+    }
+  
+    cout << "\n";
   }
 }
