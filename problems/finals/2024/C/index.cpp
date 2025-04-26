c9a424a531bdb25711f812a7791e51c8e62649c4
Miguel Rocha <miguelrocha.dev@gmail.com>
2025-04-26 17:26:51 +0100

    feat: add incomplete problem final 2024 C part III

--- problems/finals/2024/C/index.cpp
+++ problems/finals/2024/C/index.cpp
@@ -52,13 +52,73 @@ int duel(int a, int b, vector<int>& skills) {
   }
 }
 
+int get_required_skills_to_win(int player, int n, vector<int>& duels, vector<int>& skills) {
+  int winner = duels[1];
+
+  if (player == winner) {
+    return skills[player];
+  }
+
+  int i = n + player;
+  int k = i;
+  int rival;
+  int a = 0;
+
+  vector<pair<int, int>> ranges;
+
+  while (k > 1) {
+    rival = duels[k + (k % 2 == 0 ? 1 : -1)];
+
+    vector<pair<int, int>> winning_ranges = get_winning_ranges(rival, player, skills);
+    
+    if (a == 0)
+    {
+      for (auto r : winning_ranges)
+      {
+        ranges.push_back(r);
+      }
+    }
+    else {
+      vector<pair<int, int>> new_ranges;
+
+      for (auto w : ranges)
+      {
+        for (auto z : winning_ranges) 
+        {
+          auto intersects = intersection(w, z);
+
+          if (intersects.first) {
+            new_ranges.push_back(intersects.second);
+          }
+        }
+      }
+
+      if (new_ranges.size() == 0) {
+        return -1;
+      }
+
+      ranges = new_ranges;
+    }
+
+    k = floor(k / 2);
+    a++;
+  }
+
+  return ranges[0].first;
+}
+
 int main() {
-  int p, n, s;
+  int p, n, q, s;
 
   cin >> p >> n;
 
   vector<int> duels(2 * n);
   vector<int> skills(n);
+
+  if (p == 3) 
+  {
+    cin >> q;
+  }
 
   for (int i = 0; i < n; i++)
   {
@@ -73,79 +133,51 @@ int main() {
     duels[u] = duel(duels[2 * u], duels[2 * u + 1], skills);
   }
 
-  if (p == 1) 
-  {
+  if (p == 1) {
     cout << duels[1] + 1 << "\n";
   }
-  else if (p == 2) 
-  {
-    int winner = duels[1];
-
-    for (int p = 0; p < n; p++)
-    {
-      if (p == winner) 
-      {
-        cout << (p != 0 ? " ": "") << skills[p];
-        continue;
-      }
-
-      int i = n + p;
-      int k = i;
-      int rival;
-      int a = 0;
-      bool is_possible = true;
-
-      vector<pair<int, int>> ranges;
-
-      while (k > 1) {
-        rival = duels[k + (k % 2 == 0 ? 1 : -1)];
-
-        vector<pair<int, int>> winning_ranges = get_winning_ranges(rival, p, skills);
-        
-        if (a == 0)
-        {
-          for (auto r : winning_ranges)
-          {
-            ranges.push_back(r);
-          }
-        }
-        else {
-          vector<pair<int, int>> new_ranges;
-
-          for (auto w : ranges)
-          {
-            for (auto z : winning_ranges) 
-            {
-              auto intersects = intersection(w, z);
-
-              if (intersects.first) {
-                new_ranges.push_back(intersects.second);
-              }
-            }
-          }
-
-          if (new_ranges.size() == 0) {
-            is_possible = false;
-            break;
-          }
-
-          ranges = new_ranges;
-        }
-
-        k = floor(k / 2);
-        a++;
-      }
-    
-      if (is_possible) 
-      {
-        cout << (p != 0 ? " ": "") << ranges[0].first;
-      }
-      else 
-      {
-        cout << (p != 0 ? " ": "") << -1;
-      }
+  else if (p == 2) {
+    for (int player = 0; player < n; player++) {
+      cout << (player == 0 ? "" : " ") << get_required_skills_to_win(player, n, duels, skills);
     }
   
     cout << "\n";
+  }
+  else
+  {
+    char a;
+
+    for (int i = 0; i < q; i++)
+    {
+      cin >> a;
+
+    if (a == 'B') {
+      int player;
+
+      cin >> player;
+      player--;
+
+      cout << get_required_skills_to_win(player, n, duels, skills) << "\n";
+    }
+    else if (a == 'M') {
+      int player, skill;
+
+      cin >> player >> skill;
+      player--;
+
+      skills[player] = skill;
+
+      int k = floor((player + n) / 2);
+
+      while (k >= 1) {
+        duels[k] = duel(duels[2 * k], duels[2 * k + 1], skills);
+
+        k = floor(k / 2);
+      }
+    }
+    else {
+      cout << duels[1] + 1 << "\n";
+    }
+    }
   }
 }
